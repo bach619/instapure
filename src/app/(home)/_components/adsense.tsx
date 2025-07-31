a1e006fcc5ff3e0338c1ad02c1670c9c1787b4ac
Boby Mihing <bach619@gmail.com>
2025-08-01 02:29:44 +0700

    set adsense error

--- src/app/(home)/_components/adsense.tsx
+++ src/app/(home)/_components/adsense.tsx
@@ -11,7 +11,6 @@ declare global {
 
 const Adsense = ({ format = "autorelaxed", adSlot }: { format?: string; adSlot?: string }) => {
   const adClient = "ca-pub-6435811821902528";
-  const adRef = useRef<HTMLDivElement>(null);
   const pushedRef = useRef(false);
 
   useEffect(() => {
